9af7242ce75c41dec0bd785d2bd369cd33aee62f
Aebestach <64588584+Aebestach@users.noreply.github.com>
2024-08-20 00:06:34 +0800

    Read planet data
    Read planet data

---
+++ readPlanetData.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace readData
+{
+    [KSPAddon(KSPAddon.Startup.AllGameScenes, false)]
+    public class AllPlanetInfoReader : MonoBehaviour
+    {
+        void Start()
+        {
+            List<CelestialBody> bodies = FlightGlobals.Bodies;
+
+            foreach (CelestialBody body in bodies)
+            {
+                string planetName = body.name;
+                double radius = body.Radius;
+                string gravitationalParameter = body.gravParameter.ToString("E");
+                double orbitalPeriod = body.orbit?.period ?? double.NaN;
+
+                Debug.Log($"{{\"name\": \"{planetName}\",\"Radius\": {radius}, \"GravitationalParameter\": {gravitationalParameter}, \"SiderealOrbitalPeriod\": {orbitalPeriod}, J2: 0}},");
+            }
+        }
+    }
+}
